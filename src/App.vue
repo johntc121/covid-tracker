733f26d4857989731339d0447de577af4c475376
John Cripe <jcripe@webtraxllc.com>
2022-11-14 01:14:44 -0800

    finish project

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,20 @@
 <template>
-  <router-view/>
+  <Header />
+  <div class="container mx-auto">
+    <router-view/>
+  </div>
+  
 </template>
+
+<script>
+import Header from '@/components/Header'
+
+export default  {
+  components: {
+    Header,
+  },
+}
+</script>
 
 <style>
 
